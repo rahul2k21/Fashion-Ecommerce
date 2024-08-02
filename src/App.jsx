84778263e477818a3cd39d2dd34f356219f449da
Rahul <rahulmandal2k21@gmial.com>
2024-08-02 20:18:38 +0530

    2ns commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
